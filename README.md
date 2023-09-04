2b101d5678c729762af802c1dadc7b243c64a5f8
weaming <garden.yuen@gmail.com>
2023-09-04 10:17:37 +0800

    download success

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 批量导出行者gpx数据
 
-```shell
-  yarn build
-  yarn download
-```
+1. configure user and cookie in src/config.ts
+2. `yarn; npm install typescript`
+3. `yarn build`
+4. `yarn download`
--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,15 @@
-
-const xingzhe_prefix = "https://www.imxingzhe.com";
-const user_month_info = "/api/v4/user_month_info/";
+const xingzhe_prefix = 'https://www.imxingzhe.com'
+const user_month_info = '/api/v4/user_month_info/'
 // 选择下载年份
-const year = "2022";
+const year = '2023'
 // 填写个人user_id
-const user_id = "";
+const user_id = ''
 // 填写个人xingzhe cookie
-const xingzhe_cookie = ""
-
+const xingzhe_cookie = ''
 module.exports = {
   xingzhe_prefix,
   user_month_info,
   year,
   user_id,
-  xingzhe_cookie
+  xingzhe_cookie,
 }
