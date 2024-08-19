09c1bf8ea7164f4e2c59d597d7b21f77a7f610ac
weaming <garden.yuen@gmail.com>
2024-08-19 15:20:18 +0800

    add id into file name

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,52 @@
-const clientX = require('./client');
-const config = require('./config');
-const download = require('download');
-const fs = require('fs');
+const clientX = require('./client')
+const config = require('./config')
+const download = require('download')
+const fs = require('fs')
 
 const getMonthList = (month: number) => {
-  const {
-    xingzhe_prefix, user_month_info, year, user_id,
-  } = config;
-  return clientX.get(`${xingzhe_prefix}${user_month_info}?user_id=${user_id}&year=${year}&month=${month}`);
+  const {xingzhe_prefix, user_month_info, year, user_id} = config
+  return clientX.get( `${xingzhe_prefix}${user_month_info}?user_id=${user_id}&year=${year}&month=${month}`)
+}
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
 async function main() {
   const monthList = new Array(12).fill(0).map((_, index) => {
-    return getMonthList(index + 1);
-  });
+    return getMonthList(index + 1)
+  })
   try {
-    const res = await Promise.all(monthList);
-    res.forEach(item => {
-      if(item.data?.data?.wo_info.length > 0) {
-        item.data?.data?.wo_info.forEach(async (item: any)=> {
-          const url = ` https://www.imxingzhe.com/xing/${item?.id}/gpx/`;
-          const { data } = await clientX.get(url);
+    const res = await Promise.all(monthList)
+    res.forEach((item, index) => {
+      console.log(`Total ${item.data?.data?.wo_info.length} records in month ${index+1}.`)
+      if (item.data?.data?.wo_info.length > 0) {
+        item.data?.data?.wo_info.forEach(async (item: any, index: number) => {
+          let output = `${__dirname}/../download/${item?.title}-${item?.id}.gpx`;
+          if (fs.existsSync(output)) {
+            console.log(index+1, item?.title, '✔️')
+            return
+          }
+
+          const url = ` https://www.imxingzhe.com/xing/${item?.id}/gpx/`
+          const {data} = await clientX.get(url)
           // 务必使用buffer，不要使用JSON.stringfy()
-          fs.writeFile(`${__dirname}/../download/${item?.title}.gpx`, Buffer.from(data), (err: any) => {
-            if(err) {
-              console.log(err, 'err-----');
-            }
-          });
-          console.log(item?.title)
+          fs.writeFile(
+            output,
+            Buffer.from(data),
+            (err: any) => {
+              if (err) {
+                console.log(err, 'err-----')
+              }
+            },
+          )
+          console.log(index+1, item?.title, '☑️')
         })
       }
-    });
-  } catch(err) {
+    })
+  } catch (err) {
     console.log('this err', err)
   }
 }
 
-main();
+main()
